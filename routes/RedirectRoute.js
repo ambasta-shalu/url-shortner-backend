81e53ebcae582730e9a8d13cd6a2dcf426ffd9ad
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-18 15:52:33 +0530

    redirect route funtion implemented

--- routes/RedirectRoute.js
+++ routes/RedirectRoute.js
@@ -4,19 +4,20 @@ const UrlModel = require("../models/UrlModel");
 // creating express route handler
 const router = express.Router();
 
-router.get("/:code", async (req, res) => {
+router.get("/url/:code", async (req, res) => {
   try {
     // find a document match to the code in req.params.code
     const url = await UrlModel.findOne({
       urlCode: req.params.code,
     });
+
     if (url) {
-      // when valid we perform a redirect
-      return res.redirect(url.longUrl);
-    } else {
-      // else return a not found 404 status
-      return res.status(404).json("No URL Found");
+      return res.json(url);
     }
+
+    return res.status(404).json({
+      msg: "url not found",
+    });
   } catch (error) {
     // exception handler
     console.error(error);
